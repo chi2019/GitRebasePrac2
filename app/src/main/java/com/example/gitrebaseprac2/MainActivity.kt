70d178315685d26057310f171fb5c5e8aaa2d181
lucifer@2019 <cm20142019@gmail.com>
2019-08-21 22:49:22 -0500

    b-3

--- app/src/main/java/com/example/gitrebaseprac2/MainActivity.kt
+++ app/src/main/java/com/example/gitrebaseprac2/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 
 // branch-1 1st commit
 // branch-1 2nd commit
+// branch-1 3rd commit
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
